4cad60f4caaa10ab4f32b422238c38fb7811db5e
Miguel Tornero <a00820449@itesm.mx>
2022-09-02 16:46:04 -0500

    Initial commit

---
+++ ex1/ceasar.py
@@ -0,0 +1,22 @@
+import re
+
+message = input("Message: ").upper()
+
+key = input("Key: ").upper()
+
+def encode(msg, key):
+    output = ""
+    offset = ord('A')
+    for i in range(0, len(msg)):
+        if re.search(r'[A-Z]', msg[i]):
+            key_letter = key[i % len(key)]
+            new_letter_code = offset + (ord(msg[i]) + ord(key_letter) - 2*offset) % 26
+            output += chr(new_letter_code)
+        else:
+            output += msg[i]
+    return output
+
+print("------OUTPUT------")
+print("key:", key)
+print("message:", message)
+print("encoded:", encode(message, key))
