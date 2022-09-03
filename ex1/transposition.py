f695e3bf40ddc4f778d296c18838ff50a2e76dc3
Miguel Tornero <a00820449@itesm.mx>
2022-09-02 20:28:40 -0500

    added transposition.py

---
+++ ex1/transposition.py
@@ -0,0 +1,44 @@
+import re
+import numpy as np
+
+columns = 5
+mat_data = [[]]
+
+msg = re.sub(r'\s', "", input("Message? ").upper())
+key = re.sub(r'\s', "", input("Key? (No repeating letters supported) ").upper())
+
+
+orphaned = len(msg) % columns
+if orphaned > 0:
+    for i in range(0, 5 - orphaned):
+        pad_letter = ord('A') + (i % 26)
+        msg += chr(pad_letter)
+
+print("\nKey:", key)
+print("Message:", msg)
+
+keys = list(map(ord, key))
+sorted_keys = sorted(keys)
+
+i = 0
+j = 0
+for letter_index in range(0, len(msg)):
+    #print("i: {}, j: {}".format(i,j))
+    if j >= columns:
+        j = 0
+        i += 1
+        mat_data.append([])
+    mat_data[i].append(msg[letter_index])
+    j += 1
+
+mat = np.array(mat_data)
+
+outout = ""
+for n in sorted_keys:
+    column = keys.index(n)
+    if column < columns:
+        for letter in mat[:,column]:
+            if letter != "x":
+                outout += letter
+
+print("Encoded:", outout)
