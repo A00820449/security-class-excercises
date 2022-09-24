294b6b374363be62194a2d76fb3f58434bdcf72e
Miguel Tornero <a00820449@itesm.mx>
2022-09-24 15:15:05 -0500

    started hw 7

---
+++ hw7/client.py
@@ -0,0 +1,25 @@
+import socket
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+
+public_key_pem = ""
+with open("public.pem", "r") as file:
+    public_key_pem = file.read()
+
+public_key = RSA.import_key(public_key_pem)
+
+encryptor = PKCS1_OAEP.new(public_key)
+
+#message = "this is my very secret message"
+message = input("message? ")
+
+encrypted = encryptor.encrypt(message.encode("utf-8"))
+
+print("Message:", message)
+print("Sending:", encrypted)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("127.0.0.1", 1100))
+
+s.send(encrypted)
---
+++ hw7/server.py
@@ -0,0 +1,25 @@
+import socket
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+s.bind(('127.0.0.1', 1100))
+s.listen(1)
+
+private_key_pem = ""
+with open("private.pem", "r") as file:
+    private_key_pem = file.read()
+
+private_key = RSA.import_key(private_key_pem)
+decryptor = PKCS1_OAEP.new(private_key)
+
+while True:
+    conn, addr = s.accept()
+    data = conn.recv(4096)
+    if not data: break
+
+    decrypted = decryptor.decrypt(data).decode("utf-8")
+
+    print ("Received:", data)
+    print("Decoded:", decrypted)
+    
