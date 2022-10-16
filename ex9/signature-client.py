db0c2fa17f8116e524a14b899d2287b9f18bb796
Miguel Tornero <a00820449@itesm.mx>
2022-10-16 17:41:43 -0500

    finished signature client and server

--- ex9/signature-client.py
+++ ex9/signature-client.py
@@ -6,40 +6,43 @@ from Cryptodome.Cipher import PKCS1_OAEP
 from Cryptodome.Cipher import AES
 from Cryptodome.Random import get_random_bytes
 
-message = "secret message"
+def key_encrypt(bytes: bytes, key: bytes) -> bytes:
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext = cipher.encrypt(bytes)
+    return nonce + ciphertext
+
+message = "my very secret message"
 
 client_keypair = RSA.generate(1024)
 client_pubkey = client_keypair.public_key().export_key()
 signer = pkcs1_15.new(client_keypair)
 
 session_key = get_random_bytes(16)
-cipher = AES.new(session_key, AES.MODE_EAX)
-nonce = cipher.nonce
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 1100))
-
 server_pub_key_pem = s.recv(4096)
+
 print("Received server public key:\n", server_pub_key_pem.decode())
 server_pub_key = RSA.import_key(server_pub_key_pem.decode())
 encryptor = PKCS1_OAEP.new(server_pub_key)
 
 encryped_session_key = encryptor.encrypt(session_key)
-
 print("Sending session key:", session_key.hex())
 s.send(encryped_session_key)
+s.recv(4096)
 
-encrypted_client_public_key = cipher.encrypt(client_pubkey)
-msg = nonce + encrypted_client_public_key
+msg = key_encrypt(client_pubkey, session_key)
 print("Sending encrypted public key:\n", client_pubkey.decode())
 s.send(msg)
+s.recv(4096)
 
-'''
 h = SHA256.new(message.encode())
 signature = signer.sign(h)
-#print("signature len:", len(signature))
-#message_and_signature = signature + message.encode()
-#encrypted_message_and_signature = encryptor.encrypt(message_and_signature)
-#print("Sending encrypted message with signature:", encrypted_message_and_signature.hex())
-#s.send(encrypted_message_and_signature)
-'''
+print("signature len:", len(signature))
+message_and_signature = signature + message.encode()
+msg = key_encrypt(message_and_signature, session_key)
+print("Sending signed message:", message)
+s.send(msg)
+s.recv(4096)
--- ex9/signature-server.py
+++ ex9/signature-server.py
@@ -15,7 +15,7 @@ server_keypair = RSA.generate(1024)
 decryptor = PKCS1_OAEP.new(server_keypair)
 server_pubkey = server_keypair.public_key().export_key()
 
-def decrypt(bytes: bytes, key: bytes):
+def key_decrypt(bytes: bytes, key: bytes):
     ciphertext = bytes[16:]
     nonce = bytes[:16]
     cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
@@ -24,6 +24,7 @@ def decrypt(bytes: bytes, key: bytes):
     return output
 
 while True:
+    print("Waiting for connection...")
     conn, addr = s.accept()
 
     print("Sending server public key:\n", server_pubkey.decode())
@@ -32,18 +33,25 @@ while True:
     encrypted_session_key = conn.recv(4096)
     session_key = decryptor.decrypt(encrypted_session_key)
     print("Received session key:", session_key.hex())
+    conn.send(b'ok')
 
     encrypted_client_public_key_nonce = conn.recv(4096)
-    
-    client_public_key = decrypt(encrypted_client_public_key_nonce, session_key)
-    print("Received client public key:\n", client_public_key.decode())
-
-    '''
-    client_public_key_pem = conn.recv(4096)
-    client_public_key = RSA.import_key(client_public_key_pem)
-    verifier = pkcs1_15.new(client_public_key)
+    client_public_key_pem = key_decrypt(encrypted_client_public_key_nonce, session_key)
     print("Received client public key:\n", client_public_key_pem.decode())
+    client_key = RSA.import_key(client_public_key_pem)
+    verifier = pkcs1_15.new(client_key)
+    conn.send(b'ok')
 
     encrypted_message_and_signature = conn.recv(4096)
-    print(encrypted_message_and_signature.hex())
-    '''
+    message_and_signature = key_decrypt(encrypted_message_and_signature, session_key)
+    signature = message_and_signature[:128]
+    messsage = message_and_signature[128:]
+    print("Received message:", messsage.decode())
+    conn.send(b'ok')
+
+    try:
+        h = SHA256.new(messsage)
+        verifier.verify(h, signature)
+        print("Valid signature")
+    except (ValueError, TypeError):
+        print("Not valid signature")
