fe453a0b24084db38226eeb082e6c90a3239a3c3
Miguel Tornero <a00820449@itesm.mx>
2022-10-16 02:14:39 -0500

    progress on ex9

--- ex9/client.py
+++ ex9/client.py
@@ -1,11 +1,45 @@
 import socket
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Hash import HMAC, SHA256
+from Cryptodome.Signature import pkcs1_15
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+
+message = "secret message"
+
+client_keypair = RSA.generate(1024)
+client_pubkey = client_keypair.public_key().export_key()
+signer = pkcs1_15.new(client_keypair)
+
+session_key = get_random_bytes(16)
+cipher = AES.new(session_key, AES.MODE_EAX)
+nonce = cipher.nonce
+
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1", 1100))
-from_server = s.recv(4096)
-print ("Recieved from server: ",from_server.decode("ascii"))
-s.send(b"I have received msg")
+
+server_pub_key_pem = s.recv(4096)
+print("Received server public key:\n", server_pub_key_pem.decode())
+server_pub_key = RSA.import_key(server_pub_key_pem.decode())
+encryptor = PKCS1_OAEP.new(server_pub_key)
+
+encryped_session_key = encryptor.encrypt(session_key)
+
+print("Sending session key:", session_key.hex())
+s.send(encryped_session_key)
+
+encrypted_client_public_key = cipher.encrypt(client_pubkey)
+msg = nonce + encrypted_client_public_key
+print("Sending encrypted public key:\n", client_pubkey.decode())
+s.send(msg)
 
 '''
-data=s.send(b'Hola XYZ Message')
-s.close()
+h = SHA256.new(message.encode())
+signature = signer.sign(h)
+#print("signature len:", len(signature))
+#message_and_signature = signature + message.encode()
+#encrypted_message_and_signature = encryptor.encrypt(message_and_signature)
+#print("Sending encrypted message with signature:", encrypted_message_and_signature.hex())
+#s.send(encrypted_message_and_signature)
 '''
--- ex9/server.py
+++ ex9/server.py
@@ -1,19 +1,49 @@
+from pydoc import cli
 import socket
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import AES
+from Cryptodome.Hash import HMAC, SHA256
+from Cryptodome.Signature import pkcs1_15
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.Random import get_random_bytes
+
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM);
 s.bind(('127.0.0.1', 1100))
 s.listen(1)
 
+server_keypair = RSA.generate(1024)
+decryptor = PKCS1_OAEP.new(server_keypair)
+server_pubkey = server_keypair.public_key().export_key()
+
+def decrypt(bytes: bytes, key: bytes):
+    ciphertext = bytes[16:]
+    nonce = bytes[:16]
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+
+    output = cipher.decrypt(ciphertext)
+    return output
+
 while True:
     conn, addr = s.accept()
-    conn.send(b"I will send key ")
-    data = conn.recv(4096)
-    if not data: break
-    print ("Received from client: ",data.decode("ascii"))
+
+    print("Sending server public key:\n", server_pubkey.decode())
+    conn.send(server_pubkey)
     
-'''
-connection,address=s.accept();
-with connection:
-    a=connection.recv(1024);
-    print(a.decode("ascii"))
-    connection.close();
-'''
+    encrypted_session_key = conn.recv(4096)
+    session_key = decryptor.decrypt(encrypted_session_key)
+    print("Received session key:", session_key.hex())
+
+    encrypted_client_public_key_nonce = conn.recv(4096)
+    
+    client_public_key = decrypt(encrypted_client_public_key_nonce, session_key)
+    print("Received client public key:\n", client_public_key.decode())
+
+    '''
+    client_public_key_pem = conn.recv(4096)
+    client_public_key = RSA.import_key(client_public_key_pem)
+    verifier = pkcs1_15.new(client_public_key)
+    print("Received client public key:\n", client_public_key_pem.decode())
+
+    encrypted_message_and_signature = conn.recv(4096)
+    print(encrypted_message_and_signature.hex())
+    '''
